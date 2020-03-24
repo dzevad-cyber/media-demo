76820ff165dac144d612500aa101e1203c02acc7
unknown <dzevad.del@gmail.com>
2020-03-24 17:52:37 +0100

    Updates

---
+++ precache-manifest.372f36a77f77a1b7fa84186e843839ef.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4970aca6bcaa6e50178e10ebf85ef320",
+    "url": "/media-demo/index.html"
+  },
+  {
+    "revision": "68759de56a2cb68adcff",
+    "url": "/media-demo/static/css/main.abd48fb2.chunk.css"
+  },
+  {
+    "revision": "0626871e95f4c7c7a5ef",
+    "url": "/media-demo/static/js/2.45c91e9f.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/media-demo/static/js/2.45c91e9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68759de56a2cb68adcff",
+    "url": "/media-demo/static/js/main.27010fb9.chunk.js"
+  },
+  {
+    "revision": "ba00efa64a73ce65969e",
+    "url": "/media-demo/static/js/runtime-main.8b4fa690.js"
+  },
+  {
+    "revision": "73d96c4934ee2b4870daaf3b7580093b",
+    "url": "/media-demo/static/media/account_circle-24px.73d96c49.svg"
+  },
+  {
+    "revision": "a7416e4a547649ed835f7810acf43941",
+    "url": "/media-demo/static/media/album-24px.a7416e4a.svg"
+  },
+  {
+    "revision": "ff505f7bf8c47c95517e5acaa7441b9d",
+    "url": "/media-demo/static/media/all-black.ff505f7b.ff505f7b.jpg"
+  },
+  {
+    "revision": "6a56ba0405d5a6cda3dd2caa2f8c48f7",
+    "url": "/media-demo/static/media/arrow-left-24px.6a56ba04.svg"
+  },
+  {
+    "revision": "79c4f51b0d9c95cd08960c9f0d56281f",
+    "url": "/media-demo/static/media/bohemia.79c4f51b.79c4f51b.jpg"
+  },
+  {
+    "revision": "902da8e25df6354a2581b8ae851c94f3",
+    "url": "/media-demo/static/media/browse.902da8e2.svg"
+  },
+  {
+    "revision": "9854a401e889c8d8f0226fa1093698c5",
+    "url": "/media-demo/static/media/diamonds.9854a401.9854a401.jpg"
+  },
+  {
+    "revision": "b952c73f69fec88924112b5f672c8792",
+    "url": "/media-demo/static/media/favorite-24px.b952c73f.svg"
+  },
+  {
+    "revision": "726a8deccd36e485f6d034f5b8e30ca3",
+    "url": "/media-demo/static/media/gallan.726a8dec.726a8dec.jpg"
+  },
+  {
+    "revision": "b06da1fef0de6de0f72045ed541c3769",
+    "url": "/media-demo/static/media/guru.b06da1fe.b06da1fe.jpg"
+  },
+  {
+    "revision": "d9b06de9c4288d366ab61c0dc60b357f",
+    "url": "/media-demo/static/media/heart.d9b06de9.svg"
+  },
+  {
+    "revision": "e476ce3770d139e76a5425e021417b53",
+    "url": "/media-demo/static/media/jaani.e476ce37.e476ce37.jpg"
+  },
+  {
+    "revision": "5a535a99aed47b4336c68635f1754fbe",
+    "url": "/media-demo/static/media/jassmanak.5a535a99.5a535a99.jpg"
+  },
+  {
+    "revision": "264dd1fae53f1d6e86892d199c1c563e",
+    "url": "/media-demo/static/media/jazzyb.264dd1fa.264dd1fa.jpg"
+  },
+  {
+    "revision": "60e2475b4b178803c125e385b1ffc3c4",
+    "url": "/media-demo/static/media/needya.60e2475b.60e2475b.jpg"
+  },
+  {
+    "revision": "1edbb6f5240de9dbc4fdf94ff391e595",
+    "url": "/media-demo/static/media/only.1edbb6f5.1edbb6f5.jpg"
+  },
+  {
+    "revision": "d3ce5ff4416158d0aed294c6fba32a9c",
+    "url": "/media-demo/static/media/pause-24px.d3ce5ff4.svg"
+  },
+  {
+    "revision": "1501131e94f20c81e2030582963fd3c0",
+    "url": "/media-demo/static/media/play_arrow-24px.1501131e.svg"
+  },
+  {
+    "revision": "b873dbcb477d9d8acf5f18e87693175e",
+    "url": "/media-demo/static/media/playlist_play-24px.b873dbcb.svg"
+  },
+  {
+    "revision": "c4748714fb22b4dd2b2986a9fd8838c2",
+    "url": "/media-demo/static/media/queue_music-24px.c4748714.svg"
+  },
+  {
+    "revision": "354fe1dcc0f8f33cadaa8810d564b259",
+    "url": "/media-demo/static/media/radio-24px.354fe1dc.svg"
+  },
+  {
+    "revision": "552cb56b2888e266811a3a8a1638a973",
+    "url": "/media-demo/static/media/rehab.552cb56b.552cb56b.jpg"
+  },
+  {
+    "revision": "6e4cf8d9c36819d42bc51cf04bb120d7",
+    "url": "/media-demo/static/media/rihanna.6e4cf8d9.6e4cf8d9.jpg"
+  },
+  {
+    "revision": "f3ae4dd888b8ce3542555c8bc6bf5750",
+    "url": "/media-demo/static/media/shuffle-24px.f3ae4dd8.svg"
+  },
+  {
+    "revision": "8077a14354f2e3b83111746be0c3db52",
+    "url": "/media-demo/static/media/skip_next-24px.8077a143.svg"
+  },
+  {
+    "revision": "6e3b8b4531ec783119d549dbe402af4b",
+    "url": "/media-demo/static/media/skip_previous-24px.6e3b8b45.svg"
+  },
+  {
+    "revision": "8edf1653c381a64646c20d4a8d1aa0d3",
+    "url": "/media-demo/static/media/store.8edf1653.svg"
+  },
+  {
+    "revision": "60aa243c3c223aa00b6c6c7579c6a5a4",
+    "url": "/media-demo/static/media/suicide.60aa243c.60aa243c.jpg"
+  },
+  {
+    "revision": "4b38ee58059fb5769c2f9d8b5e67401c",
+    "url": "/media-demo/static/media/sukhe.4b38ee58.4b38ee58.jpg"
+  },
+  {
+    "revision": "0118bd67544cf359ced23671a003b2c5",
+    "url": "/media-demo/static/media/volume_up-24px.0118bd67.svg"
+  },
+  {
+    "revision": "d832b6f9b71faab8abc4f5fcae4fe46b",
+    "url": "/media-demo/static/media/work.d832b6f9.d832b6f9.jpg"
+  },
+  {
+    "revision": "80b7c6f4f803c348a8d9094d75600a4d",
+    "url": "/media-demo/static/media/you-da-one.80b7c6f4.80b7c6f4.jpg"
+  }
+]);
